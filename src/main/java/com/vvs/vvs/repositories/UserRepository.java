ae12056ba4b420831a7cc376a75dec1ddd6c95ca
Valongo <viniciusvalongo1@hotmail.com>
2023-06-08 02:06:49 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/vvs/vvs/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.vvs.vvs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vvs.vvs.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
