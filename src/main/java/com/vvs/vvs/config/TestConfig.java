4ce7fff236cc86f25fcbd580d52b4ea435117778
Valongo <viniciusvalongo1@hotmail.com>
2023-06-19 11:54:37 -0300

    Payment, one-to-one association

--- src/main/java/com/vvs/vvs/config/TestConfig.java
+++ src/main/java/com/vvs/vvs/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vvs.vvs.entities.Category;
 import com.vvs.vvs.entities.Order;
 import com.vvs.vvs.entities.OrderItem;
+import com.vvs.vvs.entities.Payment;
 import com.vvs.vvs.entities.Product;
 import com.vvs.vvs.entities.User;
 import com.vvs.vvs.entities.enums.OrderStatus;
@@ -83,6 +84,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:52:07z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 	}
 	
